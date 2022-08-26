db0898be8399440e290fd71da1e6645468b13432
Anant Jain <99494315+AnantJainn@users.noreply.github.com>
2022-08-26 12:46:42 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import StarOutlineIcon from "@material-ui/icons/StarOutline";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <StarOutlineIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
